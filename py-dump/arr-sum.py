5215bd3b48b12e143804e1bbee4a6c1398a8d8be
Genevieve Balestramon <51466788+httpvieve@users.noreply.github.com>
2022-02-28 23:33:56 +0800

    week 2 coding session

---
+++ py-dump/arr-sum.py
@@ -0,0 +1,13 @@
+def miniMaxSum(arr):
+    arr_sum = [0, 0, 0, 0, 0]
+    for i in range (0, len(arr)):
+        for j in range (0, len(arr)):
+                if i is not j:
+                        arr_sum[i] += arr[j]
+    print(min(arr_sum), max(arr_sum))
+    
+if __name__ == '__main__':
+        
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
---
+++ py-dump/velocity.py
@@ -0,0 +1,26 @@
+
+
+def kangaroo(x1, v1, x2, v2):
+    a = abs(v1 - x1)
+    b = abs(v2 - x1)
+    res = "NO"
+    i = 1
+    while i < 10000:
+        if x1 + a * i == x2 + b * i:
+            res = "YES"
+        i+=1
+    return result
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    print(result)
