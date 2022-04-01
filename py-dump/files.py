e244a20d8b90436eb47aa2746459c4948437f8d3
Genevieve Balestramon <genevieve_balestramon@dlsu.edu.ph>
2022-04-01 14:23:05 +0800

    learned classes and file handling

--- py-dump/files.py
+++ py-dump/files.py
@@ -1,4 +1,12 @@
-open("file.format", "cmd")
-'''
-cmd : "r" -> read, "w" -> write file
-'''
+# open files (modes)
+# 'r'  = open for read (default)
+# 'w'  = open for write, truncate
+# 'r+' = open for read/write
+# 'w+' = open for read/write, truncate
+# 'a+' = open for read/append
+oomfs = open("oomfs.txt", "r+")
+# calling methods
+oomfs.write("viray\n")
+oomfs.write("eros\n")
+names = oomfs.read()
+print(names)
