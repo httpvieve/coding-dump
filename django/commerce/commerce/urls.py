f57c31ee52cb2821fd27b036b7fe89362c4588b4
Genevieve <genevieve_balestramon@dlsu.edu.ph>
2024-09-11 05:42:08 +0800

    feat(cs50w): added django models, views, and templates for user registration

--- django/commerce/commerce/urls.py
+++ django/commerce/commerce/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for airline project.
+"""commerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,7 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("flights/",include("flights.urls")),
-    path("users/",include("users.urls"))
-
+    path("", include("auctions.urls"))
 ]
